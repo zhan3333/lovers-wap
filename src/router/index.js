73f61c9c3f724c94589fb085cbb19605d2af8da3
zhan <grianchan@gmail.com>
2017-06-09 19:25:13 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import HelloFromVue from '@/components/HelloFromVux'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/vux',
+      component: HelloFromVue
     }
   ]
 })
