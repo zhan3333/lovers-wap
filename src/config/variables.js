8adf9f7bebf3b147b66d94519c2a182f6a8ea7df
zhan <390961827@qq.com>
2017-07-12 22:49:29 +0800

    修改配置

--- src/config/variables.js
+++ src/config/variables.js
@@ -5,9 +5,9 @@ const config = {
 }
 switch (env) {
   case 'production':
-    config.apiUrl = 'http://test.com:1025/api/'
-    config.chatSocketJs = '//test.com:6001/socket.io/socket.io.js'
-    config.chatSocketUrl = '//test.com:6001'
+    config.apiUrl = 'http://api.hd.grianchan.com/api/'
+    config.chatSocketJs = '//grianchan.com:6001/socket.io/socket.io.js'
+    config.chatSocketUrl = '//grianchan.com:6001'
     break
   case 'testing':
     config.apiUrl = 'http://test.com:1025/api/'
